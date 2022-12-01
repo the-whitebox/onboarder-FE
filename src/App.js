7da8b75a2d6c8698890bba6ce825759482baee7e
Ali Zeshan <ali.zeshan@whiteboxtech.net>
2022-12-01 17:01:56 +0500

    Initial SignUp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import  Login from './components/pages/Login'; 
+import { BrowserRouter, Routes, Route } from 'react-dom'
+import  Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <Signup/>
     </div>
   );
 }
