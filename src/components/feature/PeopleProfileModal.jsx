99d8ebf61094d3303dd7063fe5fb890412e0a543
Talha Tariq <talha@whiteboxtech.net>
2022-12-07 16:51:56 +0500

    Nested Modals

---
+++ src/components/feature/PeopleProfileModal.jsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import VerticalMenu from "./VerticalMenu";
+import Divider from "@mui/material/Divider";
+import MuiGrid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+const parentModal = {
+  position: "absolute",
+  // transform: "translate(-50%, -50%)",
+  width: "95%",
+  height: "95%",
+  bgcolor: "background.paper",
+  border: "2px solid red",
+  boxShadow: 24,
+  p: 2,
+  m: 4,
+};
+
+const Grid = styled(MuiGrid)(({ theme }) => ({
+  width: "100%",
+  ...theme.typography.body2,
+  '& [role="separator"]': {
+    margin: theme.spacing(0, 3),
+  },
+}));
+
+function ChildModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button onClick={handleOpen}>Open Child Modal</Button>
+      <Modal
+        hideBackdrop
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style, width: 200 }}>
+          <h2 id="child-modal-title">Text in a child modal</h2>
+          <p id="child-modal-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+          <Button onClick={handleClose}>Close Child Modal</Button>
+        </Box>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+export default function NestedModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>People Modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...parentModal }}>
+          <Grid container>
+            <Grid item sm>
+              <VerticalMenu />
+            </Grid>
+            <Divider orientation="vertical" flexItem />
+            <Grid item sm>
+              <ChildModal />
+            </Grid>
+          </Grid>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -13,6 +13,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import PeopleProfileModal from "../feature/PeopleProfileModal";
 
 const theme = createTheme();
 
@@ -115,6 +116,7 @@ export default function SignIn() {
             </Grid>
           </Box>
         </Box>
+        <PeopleProfileModal />
       </Container>
     </ThemeProvider>
   );
