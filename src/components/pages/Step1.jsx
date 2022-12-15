7d1b510fc14a6f27724b7f0eb1d4d10cbefc038a
Talha Tariq <talha@whiteboxtech.net>
2022-12-15 20:51:16 +0500

    Step2,3

--- src/components/pages/Step1.jsx
+++ src/components/pages/Step1.jsx
@@ -16,6 +16,15 @@ import Sheet from "@mui/joy/Sheet";
 import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
 import RightSideImage from "../../assets/images/bg-image.png";
 import Item from "antd/es/list/Item";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Icon1 from "../../assets/icons/health-icon.png";
+import Icon2 from "../../assets/icons/retail-icon.png";
+import Icon3 from "../../assets/icons/services-icon.png";
+import Icon4 from "../../assets/icons/charity-icon.png";
+import Icon5 from "../../assets/icons/others-icon.png";
 
 const style = {
   position: "absolute",
@@ -31,6 +40,13 @@ const style = {
 
 export default function BasicModal() {
   const theme = useTheme();
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const icons = [Icon1, Icon2, Icon3, Icon4, Icon5];
 
   return (
     <div>
@@ -178,77 +194,119 @@ export default function BasicModal() {
                 width: "100%",
               }}
             >
-              <Item>
-                <RadioGroup
-                  aria-label="platform"
-                  defaultValue="Healthcare"
-                  overlay
-                  name="platform"
-                  sx={{
-                    mt: 2,
-                    flexDirection: "row",
-                    gap: 2,
-                    [`& .${radioClasses.checked}`]: {
-                      [`& .${radioClasses.action}`]: {
-                        inset: -1,
-                        border: "1px solid",
-                        borderColor: "#5BA5A8",
-                      },
+              <RadioGroup
+                aria-label="platform"
+                defaultValue="Website"
+                overlay
+                name="platform"
+                sx={{
+                  mt: 2,
+                  flexDirection: "row",
+                  gap: 2,
+                  [`& .${radioClasses.checked}`]: {
+                    [`& .${radioClasses.action}`]: {
+                      inset: -1,
+                      border: "3px solid",
+                      borderColor: "primary.500",
                     },
-                    [`& .${radioClasses.radio}`]: {
-                      display: "contents",
-                      "& > svg": {
-                        zIndex: 2,
-                        position: "absolute",
-                        top: "-8px",
-                        right: "-8px",
-                        bgcolor: "background.body",
-                        borderRadius: "50%",
-                      },
+                  },
+                  [`& .${radioClasses.radio}`]: {
+                    display: "contents",
+                    "& > svg": {
+                      zIndex: 2,
+                      position: "absolute",
+                      top: "-8px",
+                      right: "-8px",
+                      bgcolor: "background.body",
+                      borderRadius: "50%",
                     },
-                  }}
-                >
-                  {[
-                    "Healthcare",
-                    "Retail & Hospitality",
-                    "Services",
-                    "Charity",
-                    "Other",
-                  ].map((value) => (
-                    <Sheet
-                      key={value}
-                      variant="outlined"
-                      sx={{
-                        borderRadius: "md",
-                        bgcolor: "background.body",
-                        boxShadow: "sm",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        gap: 1.5,
-                        p: 2,
-                        width: "211px",
-                        height: "143px",
-                      }}
-                    >
-                      <Radio
-                        id={value}
-                        value={value}
-                        //   checkedIcon={<CheckCircleRoundedIcon />}
-                      />
-                      <Avatar variant="soft" size="sm" />
-                      <FormLabel
-                        htmlFor={value}
-                        sx={{ fontWeight: 600, fontSize: "15px" }}
-                      >
-                        {value}
-                      </FormLabel>
-                    </Sheet>
-                  ))}
-                </RadioGroup>
-              </Item>
+                  },
+                }}
+              >
+                {[
+                  "Healthcare",
+                  "Retail & Hospitality",
+                  "Services",
+                  "Charity",
+                  "Other",
+                ].map((value, idx) => (
+                  <Sheet
+                    key={value}
+                    variant="outlined"
+                    sx={{
+                      borderRadius: "md",
+                      bgcolor: "background.body",
+                      boxShadow: "sm",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      gap: 1.5,
+                      p: 2,
+                      minWidth: 120,
+                    }}
+                  >
+                    <Radio
+                      id={value}
+                      value={value}
+                      checkedIcon={<CheckCircleRoundedIcon />}
+                    />
+                    <Avatar variant="circular" size="sm" src={icons[idx]} />
+                    <FormLabel htmlFor={value} sx={{ fontSize: "10px" }}>
+                      {value}
+                    </FormLabel>
+                  </Sheet>
+                ))}
+              </RadioGroup>
             </Grid>
+            <Typography
+              sx={{
+                mt: 2,
+                fontWeight: "bold",
+                fontSize: "18px",
+                color: "#332A60",
+              }}
+            >
+              Select your industry
+            </Typography>
+            <FormControl
+              fullWidth
+              sx={{
+                mt: 2,
+              }}
+              className="input-border-radius"
+            >
+              <InputLabel id="demo-simple-select-label">Select</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                sx={{
+                  width: "50%",
+                }}
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <Link to="/step2">
+              <Button
+                type="submit"
+                variant="contained"
+                className="btn-forgetPwd btn-login"
+                sx={{
+                  mt: 4,
+                  width: "89px",
+                  borderRadius: "10px",
+                  justifyContent: "center",
+                }}
+              >
+                Next
+              </Button>
+            </Link>
           </Box>
         </Grid>
         <Grid item xs={false} sm={4} md={5} sx={{ backgroundColor: "#FFFFFF" }}>
