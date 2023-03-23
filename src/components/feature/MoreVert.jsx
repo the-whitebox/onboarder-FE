5602e48e393509d0d2a25dc84e3eb79e5cd59a98
ali-zeshan <119493052+ali-zeshan@users.noreply.github.com>
2023-03-23 12:10:00 +0500

    MoreVert-Dropdown

--- src/components/feature/MoreVert.jsx
+++ src/components/feature/MoreVert.jsx
@@ -1,26 +1,110 @@
-import React from "react";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
+import * as React from "react";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Link from "@mui/material/Link";
+import SetAccessLevelModalBody from "../feature/SetAccessLevel";
+import ArchiveTeamMemberModalBody from "../feature/ArchiveTeammembers";
+import SetAgreedHoursModalBody from "../feature/SetAgreedhours";
+import Modal from "@mui/material/Modal";
 
 export const MoreVert = () => {
-  const [Icon, setAge] = React.useState("");
+  const [people, setPeople] = React.useState("");
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setPeople(event.target.value);
+  };
+
+  const [openAccess, setOpenAccess] = React.useState(false);
+  const handleOpenAccess = () => setOpenAccess(true);
+  const handleCloseAccess = () => setOpenAccess(false);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [openWorkPeriod, setOpenWorkPeriod] = React.useState(false);
+  const handleOpenWorkPeriod = () => setOpenWorkPeriod(true);
+  const handleCloseWorkPeriod = () => setOpenWorkPeriod(false);
+  const modalWrapper = {
+    overflow: "auto",
+    display: "flex",
   };
   return (
-    <Select
-      labelId="demo-simple-select-label"
-      id="demo-simple-select"
-      value={Icon}
-      label="Icon"
-      onChange={handleChange}
-    >
-      <MenuItem value={1}>Agreed hours</MenuItem>
-      <MenuItem value={2}>Access level</MenuItem>
-      <MenuItem value={3}>Archive team</MenuItem>
-    </Select>
+    <React.Fragment>
+<Modal
+        open={openAccess}
+        onClose={handleCloseAccess}
+        onClick={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <SetAccessLevelModalBody />
+      </Modal>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <ArchiveTeamMemberModalBody />
+      </Modal>
+
+      <Modal
+        open={openWorkPeriod}
+        sx={modalWrapper}
+        onClose={handleCloseWorkPeriod}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <SetAgreedHoursModalBody />
+      </Modal>
+
+      <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={people}
+          onChange={handleChange}
+          label="Age"
+        >
+          <MenuItem value="">
+            <Link
+              sx={{ textDecoration: "none" }}
+              onClick={handleOpenWorkPeriod}
+              color="#38b492"
+            >
+              Agreed hours
+            </Link>
+          </MenuItem>
+
+          <MenuItem>
+            {" "}
+            <Link
+              sx={{ textDecoration: "none" }}
+              onClick={handleOpenAccess}
+              color="#38b492"
+            >
+              Access level
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            {" "}
+            <Link
+              sx={{ textDecoration: "none" }}
+              onClick={handleOpen}
+              color="#38b492"
+            >
+              
+              Archive team
+            </Link>
+          </MenuItem>
+        </Select>
+              </React.Fragment>
   );
 };
