cb732cc10a0f845a75085c052796d95e92295c75
Umar Maqsood <umar.maqsood@whiteboxtech.net>
2023-04-12 15:54:18 +0500

    feat: new navbar added

--- src/components/feature/Navbar.jsx
+++ src/components/feature/Navbar.jsx
@@ -1,31 +1,35 @@
 import * as React from "react";
+import "../../style/Navbar.css";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import MaxPilotLogo from "../../assets/images/maxpilot-logo.png";
 import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import GlobalContext from "../../context/GlobalContext";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Badge, TextField, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import TextSnippetOutlinedIcon from "@mui/icons-material/TextSnippetOutlined";
+import Person2Icon from "@mui/icons-material/Person2";
+import CorporateFareIcon from "@mui/icons-material/CorporateFare";
 
-const navLinksArr = [
-  { title: "Home", url: "/home" },
-  { title: "News Feed", url: "/news-feed" },
-  { title: "Task", url: "/task" },
-  { title: "Locations", url: "/locations" },
-  { title: "People", url: "/people" },
-  { title: "Schedule", url: "/schedule" },
-  { title: "TimeSheets", url: "/timesheets" },
-  { title: "Reports", url: "/reports" },
-  { title: "Enterprise", url: "/enterprise" },
-];
 export default function Navbar() {
   const { userInfo } = React.useContext(GlobalContext);
-  const token = localStorage.getItem("token");
   const Navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -36,10 +40,17 @@ export default function Navbar() {
   return (
     <AppBar
       className="navbar"
-      style={{ background: "#ffffff", color: "black" }}
       position="fixed"
       sx={{
         width: "100%",
+        height: "9vh",
+        px: 3,
+        background: "#ffffff",
+        color: "black",
+        boxShadow: "none",
+        filter: "drop-shadow(0px 1px 2px rgba(21,34,50,0.08 ))",
+        display: "flex",
+        justifyContent: "center",
       }}
     >
       <Box
@@ -47,70 +58,159 @@ export default function Navbar() {
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
-          py: 1,
-          px: 2,
         }}
       >
-        <Avatar
-          sx={{ width: "140px", height: "70px" }}
-          variant="square"
-          src={MaxPilotLogo}
-        />
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {navLinksArr?.map((data, index) => (
-            <NavLink to={data.url} className="navbar-aTag">
-              {data.title}
-            </NavLink>
-          ))}
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Avatar
+            sx={{ width: "auto", height: "auto" }}
+            variant="square"
+            src={MaxPilotLogo}
+          />
+          <Box
+            sx={{ width: "1px", height: "40px", bgcolor: "#707070", ml: 2 }}
+          />
+          <Typography sx={{ color: "#707070", ml: 2 }}>
+            Effortless Time Management
+          </Typography>
         </Box>
         <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              mr: 1,
-              alignItems: "flex-end",
+          <TextField
+            type="search"
+            size="small"
+            className="search-field"
+            placeholder="search..."
+            variant="standard"
+            InputProps={{
+              startAdornment: <SearchIcon sx={{ color: "#2BB491" }} />,
+              disableUnderline: true,
             }}
-          >
-            <Typography sx={{ fontSize: "0.9rem" }}>
-              {userInfo?.first_name} {userInfo?.last_name}
-            </Typography>
-            <Typography sx={{ fontSize: "0.8rem", fontWeight: "600" }}>
-              @Danish
-            </Typography>
-          </Box>
+            sx={{
+              p: "5px 10px 2px 10px",
+              display: { md: "block", xs: "none" },
+            }}
+          />
+          <Badge color="error" badgeContent={2} sx={{ mx: 3 }}>
+            <NotificationsOutlinedIcon sx={{ color: "#2BB491" }} />
+          </Badge>
           <Avatar alt="Remy Sharp" />
-          <Box sx={{ ml: 2, mr: 1, bgcolor: "black", width: 2, height: 25 }} />
-          <IconButton
-            size="medium"
-            aria-label="show 17 new notifications"
-            color="white"
-            onClick={handleLogout}
-          >
-            <SettingsOutlinedIcon />
-          </IconButton>
-
-          <IconButton
-            size="medium"
-            aria-label="show 17 new notifications"
-            color="white"
-          >
-            <FavoriteBorderIcon />
-          </IconButton>
-
-          <IconButton
-            size="medium"
-            aria-label="show 17 new notifications"
-            color="white"
-          >
-            <NotificationsOutlinedIcon />
-          </IconButton>
+          <Box>
+            <Button
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              endIcon={<KeyboardArrowDownIcon />}
+              sx={{ color: "#131523", textTransform: "none", ml: 1 }}
+            >
+              {userInfo?.first_name} {userInfo?.last_name}
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  mt: "30px",
+                  borderRadius: "11px",
+                  filter: "drop-shadow(0px 6px 3px rgba(21,34,50,0.08 ))",
+                  width: "320px",
+                  px: 2,
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <Typography
+                sx={{
+                  color: "#2BB491",
+                  fontSize: "18px",
+                  fontWeight: "600",
+                  textAlign: "center",
+                  mt: 1,
+                  mb: 1,
+                }}
+              >
+                Manage Profile
+              </Typography>
+              <Divider />
+              <Box sx={{ mt: 2, mb: 10 }}>
+                <Typography sx={{ color: "#707070", ml: 2 }}>
+                  {userInfo?.first_name} {userInfo?.last_name}
+                </Typography>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <TextSnippetOutlinedIcon sx={{ color: "black" }} />
+                  </ListItemIcon>
+                  <Box>
+                    <Typography sx={{ color: "#555555", fontSize: "15px" }}>
+                      Account Overview
+                    </Typography>
+                    <Typography sx={{ color: "#555555", fontSize: "10px" }}>
+                      Edit your Global Profile and manage billing
+                    </Typography>
+                  </Box>
+                </MenuItem>
+                <Typography sx={{ color: "#707070", mt: 3, ml: 2 }}>
+                  Business Name
+                </Typography>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Person2Icon fontSize="small" sx={{ color: "black" }} />
+                  </ListItemIcon>
+                  <Typography sx={{ color: "#555555", fontSize: "15px" }}>
+                    Profile
+                  </Typography>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <CorporateFareIcon
+                      fontSize="small"
+                      sx={{ color: "black" }}
+                    />
+                  </ListItemIcon>
+                  <Typography sx={{ color: "#555555", fontSize: "15px" }}>
+                    Business
+                  </Typography>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <CorporateFareIcon
+                      fontSize="small"
+                      sx={{ color: "black" }}
+                    />
+                  </ListItemIcon>
+                  <Typography sx={{ color: "#555555", fontSize: "15px" }}>
+                    Integration
+                  </Typography>
+                </MenuItem>
+              </Box>
+              <Divider />
+              <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  variant="contained"
+                  className="all-green-btns"
+                  sx={{
+                    mt: 2,
+                    mb: 2,
+                    textTransform: "none",
+                    width: "60%",
+                    height: 35,
+                    borderRadius: "7px",
+                  }}
+                  onClick={handleLogout}
+                >
+                  Log Out
+                </Button>
+              </Box>
+            </Menu>
+          </Box>
         </Box>
       </Box>
     </AppBar>
